767f697767b9117a52e15912c3c12c9f6b82ec0e
AsPiD21 <72015749+AsPiD21@users.noreply.github.com>
2021-12-20 08:50:01 +0300

    Delete addons/oncontrol directory

--- addons/oncontrol/pages/configuration/menu.php
+++
@@ -1,4 +0,0 @@
-<ul class="nav nav-pills nav-stacked">
-    <li><a class="list-group-item <? if($this->mod=='moduleconfig'){print 'active';}?>" onclick="$('.loading').attr('hidden',false);" href="<?=$this->vars['modulelink']?>&tabs=configuration&mod=moduleconfig"><?=$this->LANG['moduleConfigurate']?></a></li>
-    <li><a class="list-group-item <? if($this->mod=='newConfig'){print 'active';}?>" onclick="$('.loading').attr('hidden',false);" href="<?=$this->vars['modulelink']?>&tabs=configuration&mod=newConfig"><?=$this->LANG['idConfigurate']?></a></li>
-</ul>
